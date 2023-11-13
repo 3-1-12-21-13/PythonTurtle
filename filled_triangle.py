2b26ff6a5186136dc8b72a7f1745dff3d9aa214e
muircalum3 <28973946-muircalum3@users.noreply.replit.com>
2023-11-13 01:03:32 +0000

    Initial commit

---
+++ filled_triangle.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+begin_fill()
+
+forward(100)
+left(120)
+forward(100)
+left(120)
+forward(100)
+
+end_fill()
+
+done()
---
+++ red_and_green_circles.py
@@ -0,0 +1,17 @@
+from turtle import *
+
+begin_fill()
+color("red")
+circle(50)
+end_fill()
+
+penup()
+forward(100)
+pendown()
+
+begin_fill()
+color("green")
+circle(50)
+end_fill()
+
+done()
